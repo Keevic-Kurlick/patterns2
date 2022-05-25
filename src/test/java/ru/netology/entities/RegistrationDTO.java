455840d3d3415484a5af82063957596017973f55
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-26 01:07:32 +0300

    added value and allargs annotations

--- src/test/java/ru/netology/entities/RegistrationDTO.java
+++ src/test/java/ru/netology/entities/RegistrationDTO.java
@@ -1,35 +1,22 @@
 package ru.netology.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+
+@AllArgsConstructor
+@Value
 public class RegistrationDTO {
     public static final String STATUS_ACTIVE = "active";
     public static final String STATUS_BLOCKED = "blocked";
 
-    private final String login;
-    private final String password;
-    private String status;
-
-
-    public RegistrationDTO(String login, String password, String status) {
-        this.login = login;
-        this.password = password;
-        this.status = status;
-    }
+    String login;
+    String password;
+    @NonFinal
+    String status;
 
     public RegistrationDTO(String login, String password) {
         this.login = login;
         this.password = password;
-
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
     }
 }
