adaa661bae106b489201ccfb2024fba83e1a0ea0
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-26 01:09:05 +0300

    replaced api method

--- src/test/java/ru/netology/elements/Login.java
+++ src/test/java/ru/netology/elements/Login.java
@@ -1,41 +1,13 @@
 package ru.netology.elements;
 
-import io.restassured.specification.RequestSpecification;
 import ru.netology.entities.RegistrationDTO;
 
 import static com.codeborne.selenide.Selenide.$x;
-import static io.restassured.RestAssured.given;
 
 public class Login {
-    public static void login(RegistrationDTO user){
+    public static void login(RegistrationDTO user) {
         $x("//input[@name = \"login\"]").setValue(user.getLogin());
         $x("//input[@name = \"password\"]").setValue(user.getPassword());
         $x("//button[@data-test-id = \"action-login\"]").click();
     }
-
-    public static void postUser(RequestSpecification requestSpec, RegistrationDTO user) {
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
-    }
-
-    public static void postUser(RequestSpecification requestSpec, RegistrationDTO user, int statusCode) {
-        given()
-                .spec(requestSpec)
-                .body(user)
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(statusCode);
-    }
-
-    public static void changeStatus(RequestSpecification requestSpec, RegistrationDTO user, String status) {
-        user.setStatus(status);
-        postUser(requestSpec, user);
-    }
-
 }
